e745f619381c815707475695baaed222e9c6bfca
Александр Подрабинович <thewatcher@Aleksandrs-Mac-Pro.local>
2014-12-27 15:53:44 +0300

    Первый коммит конвертера возраста человека в собачий возраст

--- HumanYeatsToDogYears/ViewController.swift
+++ HumanYeatsToDogYears/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var humanAgeInput: UITextField!
+    @IBOutlet weak var dogAgeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertHumanToDogYearsButtonPressed(sender: UIButton) {
+        humanAgeInput.resignFirstResponder()
+        dogAgeLabel.textColor = UIColor.blueColor()
+        
+        let conversionVal = 7.5
+        
+        dogAgeLabel.text = "Возраст собаки: \(Double((humanAgeInput.text as NSString).doubleValue) / conversionVal)"
+    }
 
 }
 
